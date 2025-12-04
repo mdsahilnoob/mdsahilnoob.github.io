05663834c635f33f65f9090db49e6d1d72e8ee89
MD SAHIL <144834170+mdsahilnoob@users.noreply.github.com>
2025-12-04 17:53:09 +0530

    fix: http to https for security

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -20,7 +20,7 @@ export const GET: APIRoute = async ({ site }) => {
   const lastmod = new Date().toISOString();
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
 ${staticPages.map(page => `  <url>
     <loc>${siteUrl}${page.url}</loc>
     <lastmod>${lastmod}</lastmod>
